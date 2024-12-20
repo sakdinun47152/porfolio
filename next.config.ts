533129aba96dfc3cf727d3860457d3b93f8ce5ca
sakdinun47152 <bwc47152@gmail.com>
2024-12-20 15:44:21 +0700

    deploy test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
   assetPrefix: "/portfolio/",
+  images:{
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
